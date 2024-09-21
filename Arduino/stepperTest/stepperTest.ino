dbf1ca0184374373e4efb256ee69b944db3d9102
KyraCoding <pixelesc.com@gmail.com>
2024-09-21 10:29:36 -0400

    link between python and arduino

--- Arduino/stepperTest/stepperTest.ino
+++ Arduino/stepperTest/stepperTest.ino
@@ -3,32 +3,38 @@
 
 // Number of steps per output rotation
 const int stepsPerRevolution = 200;
-
+String command;
 // Create Instance of Stepper library
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-
 Stepper myStepper2(stepsPerRevolution, 4, 5, 6, 7);
 
 void setup()
 {
-	// set the speed at 60 rpm:
-	myStepper.setSpeed(60);
-  myStepper2.setSpeed(60);
-	// initialize the serial port:
-	Serial.begin(9600);
+    // Set the speed at 60 rpm
+    myStepper.setSpeed(60);
+    myStepper2.setSpeed(60);
+    // Initialize the serial port
+    Serial.begin(115200);
+    pinMode(13, OUTPUT);
 }
 
 void loop() 
 {
-	// step one revolution in one direction:
-	Serial.println("clockwise");
-	myStepper.step(stepsPerRevolution);
-  myStepper2.step(stepsPerRevolution);
-	delay(500);
-
-	// step one revolution in the other direction:
-	Serial.println("counterclockwise");
-	myStepper.step(-stepsPerRevolution);
-  myStepper2.step(-stepsPerRevolution);
-	delay(500);
+    if (Serial.available()) {
+      command = Serial.readString();
+      command.trim();
+      if (command == "clockwise") {
+          // Move clockwise
+          digitalWrite(13, HIGH); 
+          myStepper.step(stepsPerRevolution);
+          myStepper2.step(stepsPerRevolution);
+      }
+      else if (command == "counterclockwise") {
+          // Move counterclockwise
+          digitalWrite(13, LOW);  
+          myStepper.step(-stepsPerRevolution);
+          myStepper2.step(-stepsPerRevolution);
+      }
+    } 
+    
 }
--- gui.py
+++ gui.py
@@ -1,12 +1,24 @@
 from tkinter import *
 from ctypes import windll
+import serial
+import time 
+
+arduino = serial.Serial(port='COM5',  baudrate=115200, timeout=.1)
+
 windll.shcore.SetProcessDpiAwareness(1)
 
-ws = Tk()
 
+def sendCommand(cmd):
+    print("sending command: " + cmd)
+    arduino.write(bytes(cmd, 'utf-8'))
+
+ws = Tk()
 ws.title("PennApps 2024 Project")
-Tk.geometry(ws, "1440x900")
-message = Label(ws,  text="PennApps24")
+Tk.geometry(ws, "720x450")
+message = Label(ws, text="PennApps24")
 message.pack()
+Button(ws, text="clockwise", command=lambda: sendCommand("clockwise")).pack()
+Button(ws, text="counterclockwise",command=lambda: sendCommand("counterclockwise")).pack()
 
 ws.mainloop()
+
