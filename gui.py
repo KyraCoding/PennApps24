4b7d9efa84f661926a2758a41d54323c84791afc
KyraCoding <pixelesc.com@gmail.com>
2024-09-21 12:12:41 -0400

    background

--- gui.py
+++ gui.py
@@ -1,24 +1,40 @@
 from tkinter import *
 from ctypes import windll
 import serial
+from PIL import Image, ImageTk
 import time 
 
-arduino = serial.Serial(port='COM5',  baudrate=115200, timeout=.1)
+try:
+    arduino = serial.Serial(port='COM5',  baudrate=115200, timeout=.1)
+except:
+    print("Failed to connect to Arduino!")
 
 windll.shcore.SetProcessDpiAwareness(1)
 
-
+# Globals, constants and stuff
 def sendCommand(cmd):
     print("sending command: " + cmd)
     arduino.write(bytes(cmd, 'utf-8'))
+    
+root = Tk()
 
-ws = Tk()
-ws.title("PennApps 2024 Project")
-Tk.geometry(ws, "720x450")
-message = Label(ws, text="PennApps24")
+
+# Background
+image = Image.open("assets/background.png")
+background_image = ImageTk.PhotoImage(image)
+background_label = Label(root, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# Window config
+root.title("PennApps 2024 Project")
+Tk.geometry(root, "1440x900")
+root.resizable(width=False, height=False)
+
+# Title text
+message = Label(root, text="PennApps24")
 message.pack()
-Button(ws, text="clockwise", command=lambda: sendCommand("clockwise")).pack()
-Button(ws, text="counterclockwise",command=lambda: sendCommand("counterclockwise")).pack()
+Button(root, text="clockwise", command=lambda: sendCommand("clockwise")).pack()
+Button(root, text="counterclockwise",command=lambda: sendCommand("counterclockwise")).pack()
 
-ws.mainloop()
+root.mainloop()
 
