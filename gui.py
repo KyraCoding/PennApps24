ec068d5fc5a3a16a1e7d3757f8405043b2afece6
KyraCoding <pixelesc.com@gmail.com>
2024-09-21 07:31:03 -0400

    so we in the mine

---
+++ gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from ctypes import windll
+windll.shcore.SetProcessDpiAwareness(1)
+
+ws = Tk()
+
+ws.title("PennApps 2024 Project")
+Tk.geometry(ws, "1440x900")
+message = Label(ws, text="Hello, World!")
+message.pack()
+
+ws.mainloop()
