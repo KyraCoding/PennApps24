7d38bba08b561525020092aaa8319dfe952204ea
KyraCoding <pixelesc.com@gmail.com>
2024-09-21 11:37:13 -0400

    added steps (idk what that is)

--- Arduino/stepperTest/stepperTest.ino
+++ Arduino/stepperTest/stepperTest.ino
@@ -2,17 +2,17 @@
 #include <Stepper.h>
 
 // Number of steps per output rotation
-const int stepsPerRevolution = 200;
+const int stepsPerRevolution = 200 ;
 String command;
 // Create Instance of Stepper library
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-Stepper myStepper2(stepsPerRevolution, 4, 5, 6, 7);
+//Stepper myStepper2(stepsPerRevolution, 4, 5, 6, 7);
 
 void setup()
 {
     // Set the speed at 60 rpm
-    myStepper.setSpeed(60);
-    myStepper2.setSpeed(60);
+    myStepper.setSpeed(70);
+    //myStepper2.setSpeed(60);
     // Initialize the serial port
     Serial.begin(115200);
     pinMode(13, OUTPUT);
@@ -26,14 +26,18 @@ void loop()
       if (command == "clockwise") {
           // Move clockwise
           digitalWrite(13, HIGH); 
-          myStepper.step(stepsPerRevolution);
-          myStepper2.step(stepsPerRevolution);
+          for (int i =0;i<13;i++) {
+            myStepper.step(1);
+          }
+          //myStepper2.step(stepsPerRevolution);
       }
       else if (command == "counterclockwise") {
           // Move counterclockwise
           digitalWrite(13, LOW);  
-          myStepper.step(-stepsPerRevolution);
-          myStepper2.step(-stepsPerRevolution);
+          for (int i =0;i<13;i++) {
+            myStepper.step(-1);
+          }
+          //myStepper2.step(-stepsPerRevolution);
       }
     } 
     
